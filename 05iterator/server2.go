df0415d2216635351871eeaf06fe5eb84557e5fb
ryokawa <kawata.ryo.0518@gmail.com>
2022-10-12 11:08:17 +0000

    iterator

---
+++ 05iterator/server2.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"text/template"
+)
+
+func process(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("tmpl2.html")
+	// daysOfWeek := []string{"月", "火", "水", "木", "金", "土", "日"}
+	daysOfWeek := []string{}
+	t.Execute(w, daysOfWeek)
+}
+
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	http.HandleFunc("/process", process)
+	server.ListenAndServe()
+}
